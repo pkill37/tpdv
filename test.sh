7c4a94dfdecc4a5c07b0f1ef5c06bc263d5db97a
Fabio Maia <fabio.maia@bliq.ai>
2024-05-11 20:07:16 +0100

    Extend test.sh script

--- test.sh
+++ test.sh
@@ -8,5 +8,19 @@ clang-format --style=file:clang-format.yaml --verbose -i **/*.cpp **/*.h
 
 cppcheck **/*.cpp **/*.h
 
+make clean
 make
 
+./app -v vault.dat password author
+./app -p vault.dat password newpassword
+./app -p vault.dat newpassword password
+./app -a vault.dat password assets/entry1
+./app -a vault.dat password assets/entry2
+./app -l vault.dat password
+./app -d vault.dat password entry1 5b41362bc82b7f3d56edc5a306db22105707d01ff4819e26faef9724a2d406c9
+./app -f vault.dat password entry1
+./app -x vault.dat password
+
+./app -v vault2.dat password2 john
+./app -a vault2.dat password2 assets/entry1
+./app -l vault2.dat password2
